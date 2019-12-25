8abbf50e9927402dcb9b18eea40a9bb8a60f8866
phamngoctuong <phamngoctuong1805@gmail.com>
2019-12-26 06:15:10 +0700

    Add listTask

--- src/containers/TaskBoard/index.js
+++ src/containers/TaskBoard/index.js
@@ -5,14 +5,45 @@ import AddIcon from '@material-ui/icons/Add';
 import Grid from '@material-ui/core/Grid';
 import { withStyles} from '@material-ui/core';
 import  {STATUSES } from '../../constants';
+const listTask = [
+  {
+    id: 1,
+    title: 'Read book',
+    description: 'Read material ui book',
+    status: 0,
+  },
+  {
+    id: 2,
+    title: 'Play football',
+    description: 'With my friend',
+    status: 2,
+  },
+  {
+    id: 3,
+    title: 'Play game',
+    description: 'Alone 😊',
+    status: 1,
+  },
+];
 class TaskBoard extends Component {
   renderBoard() {
     let xhtml = null;
+    var {classes} = this.props;
     xhtml = (
       <Grid container spacing={2}>
         {STATUSES.map(status => {
+          const taskFiltered = listTask.filter(task => task.status === status.value);
           return (
-            <Grid key={status.value} md={4}>{status.label}</Grid>
+            <Grid item key={status.value} md={4}>
+              <div className={classes.status}>{classes.label}</div>
+              <div className={classes.wrapperListTask}>
+               {
+                taskFiltered.map(task => {
+                  return <h1 key={task.id}>{task.title}</h1>
+                })
+               }
+              </div>
+            </Grid>
           );
         })}
       </Grid>
